3df0a0bee07f4a13ae1cfc158af0b77309ec4307
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-11-15 11:31:04 -0500

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form({
+  firstName,
+  lastName, 
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName}/>
+      <input type="text" onChange={handleLastNameChange} value={lastName}/>
       <button type="submit">Submit</button>
     </form>
   );
 }
 
 export default Form;
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+//   const[newsletter, setNewsletter] = useState(false);
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   function handleNewsletterChange(event) {
+//     setNewsletter(event.target.checked);
+//   }
+
+//   return (
+//     <form>
+//       <label htmlFor= "newsletter">Subcribe to our Newsletter?</label>
+//       <input 
+//         type="checkbox" 
+//         id="newsletter" 
+//         onChange={handleNewsletterChange} 
+//         checked={newsletter}/>
+//       <button type="submit">Submit</button>
+//       <input type="text" value={firstName} onChange={handleFirstNameChange} />
+//       <input type="text" value={lastName} onChange={handleLastNameChange} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default Form;
